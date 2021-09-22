d433ea4439e2768fe991cb6dafd7421c5a50a19c
DevExpressExampleBot <fakeEmail>
2021-09-22 14:33:25 +0300

    autogenerate VB

--- VB/HideExportFormats/MainWindow.xaml.vb
+++ VB/HideExportFormats/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Xpf.Printing
+Imports DevExpress.Xpf.Printing
 Imports System
 Imports System.Collections.Generic
 Imports System.Globalization
@@ -17,27 +17,31 @@ Imports System.Windows.Shapes
 
 Namespace HideExportFormats
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private link As SimpleLink
-		Public Sub New()
-			InitializeComponent()
+        Private link As SimpleLink
 
-			AddHandler preview.Loaded, AddressOf OnPreviewLoaded
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            preview.Loaded += AddressOf OnPreviewLoaded
+        End Sub
 
-		Private Sub OnPreviewLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim data() As String = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+        Private Sub OnPreviewLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim data As String() = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+            link = New SimpleLink With {.DetailTemplate = CType(Resources("dayNameTemplate"), DataTemplate), .DetailCount = data.Length}
+            link.CreateDetail += Function(s, args) CSharpImpl.__Assign(args.Data, data(args.DetailIndex))
+            preview.DocumentSource = link
+            link.CreateDocument()
+        End Sub
 
-			link = New SimpleLink With {
-				.DetailTemplate = DirectCast(Resources("dayNameTemplate"), DataTemplate),
-				.DetailCount = data.Length
-			}
-			AddHandler link.CreateDetail, Sub(s, args) args.Data = data(args.DetailIndex)
+        Private Class CSharpImpl
 
-			preview.DocumentSource = link
-			link.CreateDocument()
-		End Sub
-	End Class
+            <System.Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
+    End Class
 End Namespace
